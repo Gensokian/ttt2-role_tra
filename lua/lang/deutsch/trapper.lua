b3ced2607e19b0b53a559a459de1d1119871cdd3
Tim Goll <tim@timgoll.de>
2020-02-22 12:04:31 +0100

    cleanup to new system

---
+++ lua/lang/deutsch/trapper.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("deutsch")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[TRAPPER.name] = "Fallensteller"
+L["info_popup_" .. TRAPPER.name] = [[Du bist ein Fallensteller!
+	Der Fallensteller kann gegnerische T-Fallen identifizieren und verwenden!]]
+L["body_found_" .. TRAPPER.abbr] = "Er war ein Fallensteller."
+L["search_role_" .. TRAPPER.abbr] = "Diese Person war ein Fallensteller!"
+L["target_" .. TRAPPER.name] = "Fallensteller"
+L["ttt2_desc_" .. TRAPPER.name] = [[Der Fallensteller ist ein unschuldiger Spieler, der seine Kraft Fallen auszulösen gegen die Tiere einsetzen muss!]]
---
+++ lua/lang/french/trapper.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[TRAPPER.name] = "Le Trappeur"
+L["info_popup_" .. TRAPPER.name] = [[Tu es un trappeur!
+	Le trappeur peut identifier et utiliser les pièges ennemis!]]
+L["body_found_" .. TRAPPER.abbr] = "Il était un trappeur!"
+L["search_role_" .. TRAPPER.abbr] = "Cette personne était un trappeur!"
+L["target_" .. TRAPPER.name] = "Le Trappeur"
+L["ttt2_desc_" .. TRAPPER.name] = [[Le trappeur est un joueur innocent qui doit utiliser son pouvoir pour poser des pièges contre l'ennemi!!]]
