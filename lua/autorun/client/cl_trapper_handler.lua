30c2b6207c56eb5bfd651acca022bf7dc0f9d1f2
Tim Goll <tim@timgoll.de>
2020-02-22 12:46:36 +0100

    added serverside handling

---
+++ lua/autorun/client/cl_trapper_handler.lua
@@ -0,0 +1,12 @@
+net.Receive("ttt2_role_trapper_used_tbutton", function()
+	local wasTrapperUse = net.ReadBool()
+	local butPos = Vector(
+		net.ReadFloat(),
+		net.ReadFloat(),
+		net.ReadFloat()
+	)
+
+	print("Button was used!")
+	print("Was Trapper usage: " .. tostring(wasTrapperUse))
+	PrintTable(butPos)
+end)
---
+++ lua/autorun/server/sv_trapper_handler.lua
@@ -0,0 +1,57 @@
+util.AddNetworkString("ttt2_role_trapper_used_tbutton")
+
+hook.Add("TTTTraitorButtonActivated", "ttt2_role_trapper_button_used", function(but, ply)
+	if not IsValid(but) or not IsValid(ply) or not ply:IsPlayer() then return end
+
+	-- INFORM TRAPPERS ABOUT BUTTON USE
+	if GetConVar("ttt_trapper_tbutton_show_to_trapper"):GetBool() and ply:GetSubRole() ~= ROLE_TRAPPER then
+		local players = player.GetAll()
+		local sendToPlayerTbl = {}
+
+		for i = 1, #players do
+			local p = players[i]
+
+			if p:GetSubRole() ~= ROLE_TRAPPER then continue end
+
+			sendToPlayerTbl[#sendToPlayerTbl + 1] = p
+		end
+
+		local butPos = but:GetPos()
+
+		net.Start("ttt2_role_trapper_used_tbutton")
+		net.WriteBool(true)
+		net.WriteFloat(butPos.x)
+		net.WriteFloat(butPos.y)
+		net.WriteFloat(butPos.z)
+		net.Send(sendToPlayerTbl)
+	end
+
+	-- INFORM OTHER BUTTON ROLES ABOUT TRAPPER BUTTON USE
+	if GetConVar("ttt_trapper_tbutton_show_to_others"):GetBool() and ply:GetSubRole() == ROLE_TRAPPER then
+		local players = player.GetAll()
+		local sendToPlayerTbl = {}
+
+		for i = 1, #players do
+			local p = players[i]
+
+			-- do not send info to trappers
+			if p:GetSubRole() == ROLE_TRAPPER then continue end
+
+			local roleName = roles.GetRoleByIndex(p:GetSubRole()).name
+
+			-- do not send info to players that can't use tButtons
+			if not GetConVar("ttt_" .. roleName .. "_traitor_button"):GetBool() then continue end
+
+			sendToPlayerTbl[#sendToPlayerTbl + 1] = p
+		end
+
+		local butPos = but:GetPos()
+
+		net.Start("ttt2_role_trapper_used_tbutton")
+		net.WriteBool(false)
+		net.WriteFloat(butPos.x)
+		net.WriteFloat(butPos.y)
+		net.WriteFloat(butPos.z)
+		net.Send(sendToPlayerTbl)
+	end
+end)
