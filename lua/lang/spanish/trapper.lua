e1a1c91b2494ccf4466f659feda35598c5b2d916
Tekiad <tekiadp@gmail.cm>
2020-07-31 03:22:56 -0300

    Create trapper.lua
    trapper spanish translation

---
+++ lua/lang/spanish/trapper.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("spanish")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[TRAPPER.name] = "Trampero"
+L["info_popup_" .. TRAPPER.name] = [[¡Eres un Trampero!
+¡Usa tus trampas para dar una ventaja estratégica a los inocentes!]]
+L["body_found_" .. TRAPPER.abbr] = "¡Era un Trampero!"
+L["search_role_" .. TRAPPER.abbr] = "Esta persona era un Trampero."
+L["target_" .. TRAPPER.name] = "Trampero"
+L["ttt2_desc_" .. TRAPPER.name] = [[El Trampero es un inocente con la habilidad de utilizar las trampas de traidores.]]
