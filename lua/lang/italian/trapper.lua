0d125ae6e27cf3c5e168b441c8b5d8e7ccbd07ae
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-09 17:46:31 +0200

    Update Italian

--- lua/lang/italian/trapper.lua
+++ lua/lang/italian/trapper.lua
@@ -1,10 +1,10 @@
 L = LANG.GetLanguageTableReference("italiano")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[TRAPPER.name] = "Trapper"
-L["info_popup_" .. TRAPPER.name] = [[Sei il Trapper!
-	Usa la tua abilità di vedere le trappole dei traditori a tuo vantagio!]]
-L["body_found_" .. TRAPPER.abbr] = "Era un Trapper!"
-L["search_role_" .. TRAPPER.abbr] = "Questa persona era un Trapper!"
-L["target_" .. TRAPPER.name] = "Trapper"
-L["ttt2_desc_" .. TRAPPER.name] = [[Il Trapper è un innocene che ha l'abilità speciale di attivare le trappole dei traditori!]]
+L[TRAPPER.name] = "Intrappolatore"
+L["info_popup_" .. TRAPPER.name] = [[Sei l'Intrappolatore!
+	Usa la tua abilità di vedere le trappole dei traditori e usarle a tuo vantagio!]]
+L["body_found_" .. TRAPPER.abbr] = "Era un Intrappolatore!"
+L["search_role_" .. TRAPPER.abbr] = "Questa persona era un Intrappolatore!"
+L["target_" .. TRAPPER.name] = "Intrappolatore"
+L["ttt2_desc_" .. TRAPPER.name] = [[l'Intrappolatore è un innocente che ha l'abilità speciale di attivare e vedere le trappole dei traditori!]]
